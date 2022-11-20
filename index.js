0700c6fd013e267bc0b0bd7a26a9931258b1cd1e
Niloy-Dey <niloydeyce@gmail.com>
2022-11-21 00:17:57 +0600

    payment method work half done

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const path = require("path");
 require("dotenv").config();
+// const SSLCommerzPayment = require('sslcommerz')
+const SSLCommerzPayment = require('sslcommerz-lts')
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // const express = require('express')
@@ -34,7 +36,8 @@ async function run() {
     const reviewCollection = client.db("taja-jinis").collection("review");
     const userCollection = client.db("taja-jinis").collection("users");
     const peopleCollection = client.db("taja-jinis").collection("allPeople");
-    
+    const completeOrderCollection = client.db("taja-jinis").collection("completeOrder");
+
     const farmerCollection = client
       .db("taja-jinis")
       .collection("farmerRequest");
@@ -131,10 +134,10 @@ async function run() {
       res.send(users);
     });
     app.post("/user", async (req, res) => {
-        const requests = req.body;
-        const result = await userCollection.insertOne(requests);
-        res.json(result);
-      });
+      const requests = req.body;
+      const result = await userCollection.insertOne(requests);
+      res.json(result);
+    });
     /* ==================================================================================== */
     /* Get order details for dashboard  */
 
@@ -169,7 +172,7 @@ async function run() {
       res.json(result);
     });
 
-    
+
     // farmer delete request from admin dashboard 
     app.delete("/farmerRequest/:id", async (req, res) => {
       const id = req.params.id;
@@ -183,9 +186,9 @@ async function run() {
 
     /////Adding All People/////////
     app.post("/allPeople", async (req, res) => {
-        const requests = req.body;
-        const result = await peopleCollection.insertOne(requests);
-        res.json(result);
+      const requests = req.body;
+      const result = await peopleCollection.insertOne(requests);
+      res.json(result);
     });
     app.post("/user", async (req, res) => {
       const requests = req.body;
@@ -223,7 +226,169 @@ async function run() {
       const isAdmin = user.role === "admin";
       res.send({ admin: isAdmin });
     });
-  } finally {
+
+
+
+
+
+
+/* ----------------------------------------------------------------
+      ssl-commerz payment method code start here 
+----------------------------------------------------------------- */
+
+//sslcommerz init
+app.post('/init', (req, res) => {
+  // console.log(req.body)
+  const data = {
+      total_amount: req.body.total_amount,
+      currency: 'BDT',
+      tran_id: 'REF123',
+      success_url: 'http://localhost:5000/success',
+      fail_url: 'http://localhost:5000/fail',
+      cancel_url: 'http://localhost:5000/cancel',
+      ipn_url: 'http://localhost:5000/ipn',
+      shipping_method: 'Courier',
+      product_name: req.body.product_name,
+      product_category: 'food',
+      product_profile: req.body.product_profile,
+      product_image: req.body.product_image,
+      cus_name: req.body.cus_name,
+      cus_email: req.body.cus_email,
+      cus_add1: 'Dhaka',
+      cus_add2: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: '1000',
+      cus_country: 'Bangladesh',
+      cus_phone: '01711111111',
+      cus_fax: '01711111111',
+      ship_name: 'Customer Name',
+      ship_add1: 'Dhaka',
+      ship_add2: 'Dhaka',
+      ship_city: 'Dhaka',
+      ship_state: 'Dhaka',
+      ship_postcode: 1000,
+      ship_country: 'Bangladesh',
+      multi_card_name: 'mastercard',
+      value_a: 'ref001_A',
+      value_b: 'ref002_B',
+      value_c: 'ref003_C',
+      value_d: 'ref004_D'
+  };
+  // console.log(data);
+  const sslcommer = new SSLCommerzPayment('tajaj6378ffa9e14bd','tajaj6378ffa9e14bd@ssl',false) //true for live default false for sandbox
+  sslcommer.init(data).then(data => {
+      //process the response that got from sslcommerz 
+      //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+      //console.log(data);
+      //res.redirect(data.GatewayPageURL)
+
+
+      // pop up node js  to frot-end  
+      if(data.GatewayPageURL){
+        res.json(data.GatewayPageURL);
+      }
+      else{
+        return res.status(400).json({
+          message:"payment session is failed"
+        })
+      }
+  });
+})
+
+
+
+  app.post('/success', async(req, res)=> {
+    // console.log(req.body);
+    res.status(200).redirect(`http://localhost:3000/success`);
+  });
+
+  app.post('/fail', async(req, res)=> {
+    // console.log(req.body);
+    res.status(400).redirect(`http://localhost:3000`);
+  });
+  
+  app.post('/cancel', async(req, res)=> {
+    // console.log(req.body);
+    res.status(200).redirect(`http://localhost:3000`);
+  });
+  
+
+
+  
+
+
+
+
+
+
+
+//   //sslcommerz init
+// app.get('/init', (req, res) => {
+//   const data = {
+//       total_amount: 100,
+//       currency: 'BDT',
+//       tran_id: 'REF123',
+//       success_url: 'http://localhost:5000/success',
+//       fail_url: 'http://localhost:5000/fail',
+//       cancel_url: 'http://localhost:5000/cancel',
+//       ipn_url: 'http://yoursite.com/ipn',
+//       shipping_method: 'Courier',
+//       product_name: 'Computer.',
+//       product_category: 'Electronic',
+//       product_profile: 'general',
+//       cus_name: 'Customer Name',
+//       cus_email: 'cust@yahoo.com',
+//       cus_add1: 'Dhaka',
+//       cus_add2: 'Dhaka',
+//       cus_city: 'Dhaka',
+//       cus_state: 'Dhaka',
+//       cus_postcode: '1000',
+//       cus_country: 'Bangladesh',
+//       cus_phone: '01711111111',
+//       cus_fax: '01711111111',
+//       ship_name: 'Customer Name',
+//       ship_add1: 'Dhaka',
+//       ship_add2: 'Dhaka',
+//       ship_city: 'Dhaka',
+//       ship_state: 'Dhaka',
+//       ship_postcode: 1000,
+//       ship_country: 'Bangladesh',
+//       multi_card_name: 'mastercard',
+//       value_a: 'ref001_A',
+//       value_b: 'ref002_B',
+//       value_c: 'ref003_C',
+//       value_d: 'ref004_D'
+//   };
+//   const sslcommer = new SSLCommerzPayment('tajaj6378ffa9e14bd', 'tajaj6378ffa9e14bd@ssl',false) //true for live default false for sandbox
+//   sslcommer.init(data).then(data => {
+//       //process the response that got from sslcommerz 
+//       //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+
+//     res.redirect(data.GatewayPageURL);
+//   });
+// })
+
+// app.post('/success', async(req, res)=> {
+//   console.log(req.body);
+//   res.status(200).json(req.body);
+// });
+
+
+// app.post('/fail', async(req, res)=> {
+//   console.log(req.body);
+//   res.status(400).json(req.body);
+// });
+
+// app.post('/cancel', async(req, res)=> {
+//   console.log(req.body);
+//   res.status(200).json(req.body);
+// });
+
+
+  } 
+  finally {
+    
   }
 }
 run().catch(console.dir);
