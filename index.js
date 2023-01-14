e6bc7d0634a7869c5703165ecea2d01d032a9f28
Niloy-Dey <niloydeyce@gmail.com>
2023-01-14 23:24:59 +0600

    problem facing

--- index.js
+++ index.js
@@ -236,82 +236,82 @@ async function run() {
       ssl-commerz payment method code start here 
 ----------------------------------------------------------------- */
 
-//sslcommerz init
-app.post('/init', async(req, res) => {
-  // console.log(req.body)
-  const data = {
-      total_amount: req.body.total_amount,
-      currency: 'BDT',
-      tran_id: 'REF123',
-      success_url: 'http://localhost:5000/success',
-      fail_url: 'http://localhost:5000/failure',
-      cancel_url: 'http://localhost:5000/cancel',
-      ipn_url: 'http://localhost:5000/ipn',
-      shipping_method: 'Courier',
-      product_name: req.body.product_name,
-      product_category: 'food',
-      product_profile: req.body.product_profile,
-      product_image: req.body.product_image,
-      cus_name: req.body.cus_name,
-      cus_email: req.body.cus_email,
-      cus_add1: 'Dhaka',
-      cus_add2: 'Dhaka',
-      cus_city: 'Dhaka',
-      cus_state: 'Dhaka',
-      cus_postcode: '1000',
-      cus_country: 'Bangladesh',
-      cus_phone: '01711111111',
-      cus_fax: '01711111111',
-      ship_name: 'Customer Name',
-      ship_add1: 'Dhaka',
-      ship_add2: 'Dhaka',
-      ship_city: 'Dhaka',
-      ship_state: 'Dhaka',
-      ship_postcode: 1000,
-      ship_country: 'Bangladesh',
-      multi_card_name: 'mastercard',
-      value_a: 'ref001_A',
-      value_b: 'ref002_B',
-      value_c: 'ref003_C',
-      value_d: 'ref004_D'
-  };
-  // console.log(data);
-  const sslcommer = new SSLCommerzPayment('tajaj6378ffa9e14bd','tajaj6378ffa9e14bd@ssl',false) //true for live default false for sandbox
-  sslcommer.init(data).then(data => {
-      //process the response that got from sslcommerz 
-      //https://developer.sslcommerz.com/doc/v4/#returned-parameters
-      //console.log(data);
-      //res.redirect(data.GatewayPageURL)
+// //sslcommerz init
+// app.post('/init', async(req, res) => {
+//   // console.log(req.body)
+//   const data = {
+//       total_amount: req.body.total_amount,
+//       currency: 'BDT',
+//       tran_id: 'REF123',
+//       success_url: 'http://localhost:5000/success',
+//       fail_url: 'http://localhost:5000/failure',
+//       cancel_url: 'http://localhost:5000/cancel',
+//       ipn_url: 'http://localhost:5000/ipn',
+//       shipping_method: 'Courier',
+//       product_name: req.body.product_name,
+//       product_category: 'food',
+//       product_profile: req.body.product_profile,
+//       product_image: req.body.product_image,
+//       cus_name: req.body.cus_name,
+//       cus_email: req.body.cus_email,
+//       cus_add1: 'Dhaka',
+//       cus_add2: 'Dhaka',
+//       cus_city: 'Dhaka',
+//       cus_state: 'Dhaka',
+//       cus_postcode: '1000',
+//       cus_country: 'Bangladesh',
+//       cus_phone: '01711111111',
+//       cus_fax: '01711111111',
+//       ship_name: 'Customer Name',
+//       ship_add1: 'Dhaka',
+//       ship_add2: 'Dhaka',
+//       ship_city: 'Dhaka',
+//       ship_state: 'Dhaka',
+//       ship_postcode: 1000,
+//       ship_country: 'Bangladesh',
+//       multi_card_name: 'mastercard',
+//       value_a: 'ref001_A',
+//       value_b: 'ref002_B',
+//       value_c: 'ref003_C',
+//       value_d: 'ref004_D'
+//   };
+//   // console.log(data);
+//   const sslcommer = new SSLCommerzPayment('tajaj6378ffa9e14bd','tajaj6378ffa9e14bd@ssl',false) //true for live default false for sandbox
+//   sslcommer.init(data).then(data => {
+//       //process the response that got from sslcommerz 
+//       //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+//       //console.log(data);
+//       //res.redirect(data.GatewayPageURL)
 
 
-      // pop up node js  to frot-end  
-      if(data?.GatewayPageURL){
-        res.json(data?.GatewayPageURL);
-      }
-      else{
-        return res.status(400).json({
-          message:"payment session is failed"
-        })
-      }
-  });
-})
+//       // pop up node js  to frot-end  
+//       if(data?.GatewayPageURL){
+//         res.json(data?.GatewayPageURL);
+//       }
+//       else{
+//         return res.status(400).json({
+//           message:"payment session is failed"
+//         })
+//       }
+//   });
+// })
 
 
 
-  app.post('/success', async(req, res)=> {
-    // console.log(req.body);
-    res.status(200).redirect(`http://localhost:3000/success`);
-  });
+  // app.post('/success', async(req, res)=> {
+  //   // console.log(req.body);
+  //   res.status(200).redirect(`http://localhost:3000/success`);
+  // });
 
-  app.post('/fail', async(req, res)=> {
-    // console.log(req.body);
-    res.status(400).redirect(`http://localhost:3000`);
-  });
+  // app.post('/fail', async(req, res)=> {
+  //   // console.log(req.body);
+  //   res.status(400).redirect(`http://localhost:3000`);
+  // });
   
-  app.post('/cancel', async(req, res)=> {
-    // console.log(req.body);
-    res.status(200).redirect(`http://localhost:3000`);
-  });
+  // app.post('/cancel', async(req, res)=> {
+  //   // console.log(req.body);
+  //   res.status(200).redirect(`http://localhost:3000`);
+  // });
   
 
 
